ad94fe283acb3538eee02500e75631b4f95e8032
lnotspotl <lnotspotl@gmail.com>
2025-02-22 14:13:40 -0500

    remove test file with no tests

--- backend/test/conftest.py
+++
@@ -1,8 +0,0 @@
-# backend/test/conftest.py
-import pytest
-import sys
-from pathlib import Path
-
-@pytest.fixture(autouse=True)
-def add_path():
-    sys.path.append(str(Path(__file__).parent.parent))
