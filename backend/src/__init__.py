725ad270fc576ad830c25367a58397c0ee81389b
Md Jakaria <jakaria.dcc11@gmail.com>
2025-04-11 22:17:57 -0400

    auto-commit

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -3,7 +3,6 @@ import pyrebase
 config = {
     # Firebase configuration
     # Replace with your actual Firebase project configuration
-
     # "apiKey": 
     # "authDomain": 
     # "databaseURL": 
@@ -12,5 +11,13 @@ config = {
     # "messagingSenderId":
     # "appId": 
     # "measurementId":
+    "apiKey": "AIzaSyA7v6-wPN87g-0SGXVMCV3oanOIkfpGvdU",
+    "authDomain": "flashcards-2ccd7.firebaseapp.com",
+    "databaseURL": "https://flashcards-2ccd7-default-rtdb.firebaseio.com",
+    "projectId": "flashcards-2ccd7",
+    "storageBucket": "flashcards-2ccd7.firebasestorage.app",
+    "messagingSenderId": "642654331718",
+    "appId": "1:642654331718:web:734e147c53aeec66fffb47",
+    "measurementId": "G-X9XFJP58PX"
 }
 firebase = pyrebase.initialize_app(config)
